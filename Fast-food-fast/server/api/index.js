42b085b80904ac12f63f88b0bc302b31bc145969
barema4 <samrubarema6@gmail.com>
2019-08-20 16:31:24 +0300

    Add user and transaction models

---
+++ Fast-food-fast/server/api/index.js
@@ -0,0 +1,31 @@
+const User = require('../../server/models/users')
+
+module.exports = function (router) {
+    router.get('/user/:id', function (req,res){
+        User.findById(req.params.id).exec()
+        .then(docs => res.status(200).json(docs))
+
+        .catch(err => res.status(500).json({
+            message: 'Error finding user',
+            error: err
+        }))
+    })
+
+    router.get('/user/email/:email', function (req,res){
+        User.findById(req.params.email).exec()
+        .then(docs => res.status(200).json(docs))
+
+        .catch(err => res.status(500).json({
+            message: 'Error finding user',
+            error: err
+        }))
+    })
+
+    router.post('/user', function (req, res) {
+        let user = new User(req.body)
+        user.save(function (err, user){
+            if (err) return console.log(err)
+            res.status(200).json(user)
+        })
+    })
+}
